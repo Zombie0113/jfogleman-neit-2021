18ddbfed69224fb97ce650b4f2822d99de9e62b7
Fogleman <jjfogleman@labs.neit.edu>
2021-10-07 14:25:26 -0400

    Level 1 Miniboss Complete + fix for Level 1-3

--- Level1/Level1MB/js/game.js
+++ Level1/Level1MB/js/game.js
@@ -7,7 +7,7 @@ var ball = new Ball();
 
 //Set the ball's velocity
 ball.vx = 1;
-ball.vy = 0;
+ball.vy = 1;
 
 function animate()
 {
@@ -17,14 +17,16 @@ function animate()
     //Move the ball
     ball.move();
 
-    //Check for the boundaries and reverse the velocity
-    if (ball.x > canvas.width - ball.width/2)
+    //Check for the boundaries of x and reverse the velocity
+    if (ball.x > canvas.width - ball.width/2 || ball.x < 0 + ball.width/2)
     {
         ball.vx = -ball.vx;
     }
-    if (ball.x < 0 + ball.width/2)
+
+    //Check for the boundaries of y and reverse the velocity
+    if (ball.y > canvas.height - ball.height/2 || ball.y < 0 + ball.height/2)
     {
-        ball.vx = -ball.vx;
+        ball.vy = -ball.vy;
     }
 
     //Draw the ball
