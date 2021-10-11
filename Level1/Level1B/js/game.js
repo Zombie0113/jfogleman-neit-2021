e6ce1dfc3dcf52144ea2e3fd1ebd6001239d4eb5
Fogleman <jjfogleman@labs.neit.edu>
2021-10-11 13:56:19 -0400

    Level 1 Boss

--- Level1/Level1B/js/game.js
+++ Level1/Level1B/js/game.js
@@ -3,11 +3,20 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");	
 var timer = setInterval(animate, interval);
 var interval = 1000/60;
-var ball = new Ball();
+var ball = new Ball(); 
+var randomNum;
+const randomColors = new Array("#a20000", "#a15203", "#a1a101", "#ff0000", "#336601", "#088446", "#008282", "#004183", "#0121cc", "#450a80", "#6a016a", "#99014e");
+const nameColors = new Array("Dark Red", "Umber Brown", "Mustard Yellow", "Bright Red", "Olive Green", "Green", "Teal", "Navy Blue", "Blue", "Purple", "Violet", "Fuschia");
 
 //Set the ball's velocity
 ball.vx = 1;
 ball.vy = 1;
+
+function resetBall() 
+{
+    ball.x = canvas.width/2;
+	ball.y = canvas.height/2;
+}
 
 function animate()
 {
@@ -20,15 +29,19 @@ function animate()
     //Check for the boundaries of x and reverse the velocity
     if (ball.x > canvas.width - ball.width/2 || ball.x < 0 + ball.width/2)
     {
+        randomNum = Math.floor(Math.random() * 12)
         ball.vx = -ball.vx;
-        ball.color = "#34abeb";
+        ball.color = randomColors[randomNum];
+        document.getElementById("color").innerHTML = "Current Color: " + nameColors[randomNum];
     }
 
     //Check for the boundaries of y and reverse the velocity
     if (ball.y > canvas.height - ball.height/2 || ball.y < 0 + ball.height/2)
     {
+        randomNum = Math.floor(Math.random() * 12)
         ball.vy = -ball.vy;
-        ball.color = "#18497d";
+        ball.color = randomColors[randomNum];
+        document.getElementById("color").innerHTML = "Current Color: " + nameColors[randomNum];
     }
 
     //Draw the ball
